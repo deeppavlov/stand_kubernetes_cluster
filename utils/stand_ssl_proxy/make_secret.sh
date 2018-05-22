ad5b9c3d456adc02ee8c13cfd487a735c2702196
alitinsky <alitinsky@gmail.com>
2018-05-22 18:22:28 +0300

    fixed secret generation

--- utils/stand_ssl_proxy/make_secret.sh
+++ utils/stand_ssl_proxy/make_secret.sh
@@ -2,7 +2,10 @@
 
 SOURCE_CERT_PATH="certs/nginx.crt"
 SOURCE_KEY_PATH="certs/nginx.key"
-SECRET_CONFIG_PATH="../../kuber_configs/common/secrets/ssl_proxy/stand_ssl_proxy_st.yaml"
+SECRET_CONFIG_TEMPLATE_PATH="../../kuber_configs/common/secrets/ssl_proxy/stand_ssl_proxy_st_template.yaml"
+SECRET_CONFIG_PATH="../../kuber_configs/common/secrets/ssl_proxy/secret/stand_ssl_proxy_st.yaml"
+
+cp $SECRET_CONFIG_TEMPLATE_PATH $SECRET_CONFIG_PATH
 
 CERT=$(echo $(cat $SOURCE_CERT_PATH | base64) | sed 's/[[:space:]]//g')
 KEY=$(echo $(cat $SOURCE_KEY_PATH | base64) | sed 's/[[:space:]]//g')
