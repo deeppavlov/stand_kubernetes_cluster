d47d5c895f37d0887b21b91d3d76ca70321f9bb1
alitinsky <alitinsky@gmail.com>
2018-05-23 15:36:34 +0300

    Small fixes in ner EN

--- models/stand_ner_en/run_ner_en.sh
+++ models/stand_ner_en/run_ner_en.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export CUDA_VISIBLE_DEVICES=0 &&
 source ./env/bin/activate &&
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/ &&
 nohup python3.6 -m deeppavlov.deep riseapi configs/ner_ontonotes.json > ./stand_ner_en.log 2>&1 &
