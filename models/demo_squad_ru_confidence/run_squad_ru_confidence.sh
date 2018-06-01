8677c9de0ac640bce186bdaf7c3e0d89191c9f83
alitinsky <alitinsky@gmail.com>
2018-06-01 18:12:32 +0300

    added demo ru squad confidence

---
+++ models/demo_squad_ru_confidence/run_squad_ru_confidence.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=0 &&
+source ./env/bin/activate &&
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/ &&
+nohup python3.6 -m deeppavlov.deep riseapi DeepPavlov/deeppavlov/configs/squad/squad_ru.json > ./demo_squad_ru_confidence.log 2>&1 &
