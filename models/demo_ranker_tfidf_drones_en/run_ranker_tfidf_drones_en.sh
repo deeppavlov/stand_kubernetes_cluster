d5a215948e75d25c8be4bfb71d9bd65b82a31c06
litinsky <alitinsky@gmail.com>
2018-06-14 18:39:30 +0300

    added demo_ranker_tfidf_drones_en

---
+++ models/demo_ranker_tfidf_drones_en/run_ranker_tfidf_drones_en.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source ./env/bin/activate &&
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/ &&
+nohup python3.6 -m deeppavlov.deep riseapi DeepPavlov/deeppavlov/configs/odqa/en_ranker_tfidf_drones.json > ./demo_ranker_tfidf_drones_en.log 2>&1 &
