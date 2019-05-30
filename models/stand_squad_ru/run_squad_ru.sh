226ca9f263ef5117fb5cbd67551767c06ce5c70a
litinsky <litinsky@ipavlov.ai>
2019-05-30 18:48:02 +0300

    Modified stand_squad_ru model: changet to squad_ru_bert_infer.json config

--- models/stand_squad_ru/run_squad_ru.sh
+++ models/stand_squad_ru/run_squad_ru.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ./env/bin/activate &&
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/ &&
-nohup python3.6 -m deeppavlov.deep  riseapi    DeepPavlov/deeppavlov/configs/squad/squad_ru.json > ./stand_squad_ru.log 2>&1 &
+nohup python3.6 -m deeppavlov.deep  riseapi    DeepPavlov/deeppavlov/configs/squad/squad_ru_bert_infer.json > ./stand_squad_ru.log 2>&1 &
