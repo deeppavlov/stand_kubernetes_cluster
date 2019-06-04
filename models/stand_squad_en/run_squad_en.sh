f2d4c5fdbf0b3544b4a2efb5ec31caeeb530fa1e
litinsky <litinsky@ipavlov.ai>
2019-06-04 13:25:06 +0300

    Updated several models in stand group

--- models/stand_squad_en/run_squad_en.sh
+++ models/stand_squad_en/run_squad_en.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ./env/bin/activate &&
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/ &&
-nohup python3.6 -m deeppavlov.deep  riseapi    DeepPavlov/deeppavlov/configs/squad/squad.json > ./stand_squad_en.log 2>&1 &
+nohup python3.6 -m deeppavlov.deep  riseapi    DeepPavlov/deeppavlov/configs/squad/squad_bert_infer.json > ./stand_squad_en.log 2>&1 &
