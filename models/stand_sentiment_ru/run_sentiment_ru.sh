1cca6ea67e739c1189df8107ca3e0a4c526ef9ef
litinsky <litinsky@ipavlov.ai>
2019-06-10 15:34:36 +0300

    Updated stand_sentiment_ru model

--- models/stand_sentiment_ru/run_sentiment_ru.sh
+++ models/stand_sentiment_ru/run_sentiment_ru.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ./env/bin/activate &&
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/ &&
-nohup python3.6 -m deeppavlov.deep  riseapi    DeepPavlov/deeppavlov/configs/classifiers/rusentiment_bert.json > ./stand_sentiment_ru.log 2>&1 &
+nohup python3.6 -m deeppavlov.deep  riseapi    DeepPavlov/deeppavlov/configs/classifiers/rusentiment_elmo_twitter_cnn.json > ./stand_sentiment_ru.log 2>&1 &
