f7e0178681c41eaefe9ddc53549f08970321625e
Fedor Ignatov <ignatov.fedor@gmail.com>
2019-07-04 11:30:51 +0300

    fix: renamed variables, changed messanges and config file

--- utils/stand_monitoring/poller/poller.py
+++ utils/stand_monitoring/poller/poller.py
@@ -2,46 +2,51 @@ import json
 import polling
 import requests
 
+OK_RESPONSE = 200
 
 config_path = './config.json'
 with open(config_path, 'r') as f_config:
     config = json.load(f_config)
 
 
-def probe(stands: dict, request_timeout: float, response_pattern: int):
+def probe(services: dict, request_timeout: float):
     probe_result = {}
 
-    for url, model in stands.items():
+    for model, url in services.items():
         stand_name = ' '.join([model, url])
         try:
-            response = requests.post(url+'/poller', json={}, timeout=request_timeout)
-            probe_result[stand_name] = response.status_code == response_pattern
+            response = requests.post(f'{url}/poller', json={}, timeout=request_timeout)
+            probe_result[stand_name] = response.status_code is OK_RESPONSE
         except Exception:
             probe_result[stand_name] = False
 
     return probe_result
 
 
-def act(stands_status: dict, probe_result: dict):
-    urls = {url: probe_result[url] for url, status in stands_status.items() if (url in probe_result and
-                                                                                status is not probe_result[url])}
+def act(services_status: dict, probe_result: dict):
+    urls = {url: probe_result[url] for url, status in services_status.items() if (url in probe_result and
+                                                                                  status is not probe_result[url])}
     if urls:
         notify(urls)
 
 
-def notify(stands: dict, first_notification=False):
+def notify(services: dict, first_notification=False):
     channel = config['general']['notification']
     channel_config = config['notification'][channel]
+    msgs = config['notification']['params']
+    launch_msg = msgs['launch_msg']
+    up_msg = msgs['up_msg']
+    down_msg = msgs['down_msg']
 
-    good_stands = '\n'.join([stand for stand, status in stands.items() if status])
-    bad_stands = '\n'.join([stand for stand, status in stands.items() if not status])
+    up_services = '\n'.join([stand for stand, status in services.items() if status])
+    down_services = '\n'.join([stand for stand, status in services.items() if not status])
     notification = []
     if first_notification:
-        notification.append('THIS IS FIRST NOTIFICATION AFTER LAUNCH.')
-    if good_stands:
-        notification.append(f'Following stand endpoints are reachable:\n\n{good_stands}')
-    if bad_stands:
-        notification.append(f'Following stand endpoints are unreachable:\n\n{bad_stands}')
+        notification.append(f'{launch_msg}')
+    if up_services:
+        notification.append(f'{up_msg}\n\n{up_services}')
+    if down_services:
+        notification.append(f'{down_msg}\n\n{down_services}')
     notification = '\n\n'.join(notification)
 
     if channel == 'slack':
@@ -53,24 +58,23 @@ def notify(stands: dict, first_notification=False):
 def start_pooling():
     polling_interval = config['general']['polling_interval']
     request_timeout = config['general']['request_timeout']
-    response_pattern = config['general']['response_pattern']
-    stands = config['stands']
+    services = config['services']
 
-    stands_status = probe(stands, request_timeout, response_pattern)
-    notify(stands_status, True)
+    services_status = probe(services, request_timeout)
+    notify(services_status, True)
 
     def estimate(prob: dict):
-        return stands_status != prob
+        return services_status != prob
 
     while True:
         probe_result = polling.poll(
-            lambda: probe(stands, request_timeout, response_pattern),
+            lambda: probe(services, request_timeout),
             check_success=estimate,
             step=polling_interval,
             poll_forever=True)
 
-        act(stands_status, probe_result)
-        stands_status = probe_result
+        act(services_status, probe_result)
+        services_status = probe_result
 
 
 if __name__ == '__main__':
